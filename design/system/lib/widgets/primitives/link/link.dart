3834285c22ce3d0d6e8fd7d70b7aee8b30c1c9bf
Anton Bashirov <anton.sh.local@gmail.com>
2024-04-28 21:01:54 +0400

    fixes

--- design/system/lib/widgets/primitives/link/link.dart
+++ design/system/lib/widgets/primitives/link/link.dart
@@ -1,20 +1,16 @@
+import 'package:design/constants/web.dart';
 import 'package:design/extensions/extensions.dart';
 import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Link extends StatefulWidget {
-  static final _tracked = <Uri>{};
-
   final String text;
   final Uri uri;
   final TextStyle? textStyle;
   final Color? textColor;
-  final Color? activeTextColor;
   final Color? hoverTextColor;
   final LaunchMode? openMode;
-  final bool active;
-  final bool track;
-  final bool underlineActive;
+  final bool underlineHover;
 
   const Link({
     super.key,
@@ -24,10 +20,7 @@ class Link extends StatefulWidget {
     this.textColor,
     this.textStyle,
     this.hoverTextColor,
-    this.activeTextColor,
-    this.active = false,
-    this.track = true,
-    this.underlineActive = true,
+    this.underlineHover = true,
   });
 
   @override
@@ -39,25 +32,20 @@ class _LinkState extends State<Link> {
 
   @override
   Widget build(BuildContext context) {
-    final active = widget.active || (widget.track && Link._tracked.contains(widget.uri));
     final effectiveTextStyle = widget.textStyle ?? context.typography.base.bodyMedium;
-    final effectiveColor = active
-        ? (widget.textColor ?? context.colors.accent)
-        : _hover
-            ? (widget.hoverTextColor ?? context.colors.contentMedium)
-            : (widget.textColor ?? context.colors.contentMediumHover);
+    final effectiveColor = _hover ? (widget.hoverTextColor ?? context.colors.accent) : (widget.textColor ?? context.colors.contentMedium);
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       onEnter: (_) => setState(() => _hover = true),
       onExit: (_) => setState(() => _hover = false),
       child: GestureDetector(
-        onTap: () => launchUrl(widget.uri).whenComplete(() => setState(() => Link._tracked.add(widget.uri))),
-        onTertiaryTapUp: (_) => launchUrl(widget.uri).whenComplete(() => setState(() => Link._tracked.add(widget.uri))),
+        onTap: () => launchUrl(widget.uri),
+        onTertiaryTapUp: (_) => launchUrl(widget.uri, webOnlyWindowName: webBlank),
         child: AbsorbPointer(
           child: Text(
             widget.text,
             style: effectiveTextStyle.apply(
-              decoration: (widget.underlineActive && active) ? TextDecoration.underline : null,
+              decoration: (widget.underlineHover && _hover) ? TextDecoration.underline : null,
               decorationColor: effectiveColor,
               color: effectiveColor,
             ),
