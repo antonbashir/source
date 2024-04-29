af435059cd72b0d249ecb224a6bb1fc54e15328a
Anton Bashirov <anton.sh.local@gmail.com>
2024-04-29 12:02:47 +0400

    fixes

--- design/system/lib/widgets/primitives/link/link.dart
+++ design/system/lib/widgets/primitives/link/link.dart
@@ -3,14 +3,18 @@ import 'package:design/extensions/extensions.dart';
 import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+var _visitedLinks = <Uri>{};
+
 class Link extends StatefulWidget {
   final String text;
   final Uri uri;
   final TextStyle? textStyle;
   final Color? textColor;
-  final Color? hoverTextColor;
   final LaunchMode? openMode;
+  final Color? hoverTextColor;
   final bool underlineHover;
+  final Color? visitedTextColor;
+  final bool underlineVisited;
 
   const Link({
     super.key,
@@ -21,6 +25,8 @@ class Link extends StatefulWidget {
     this.textStyle,
     this.hoverTextColor,
     this.underlineHover = true,
+    this.visitedTextColor,
+    this.underlineVisited = false,
   });
 
   @override
@@ -29,23 +35,34 @@ class Link extends StatefulWidget {
 
 class _LinkState extends State<Link> {
   var _hover = false;
+  late var _visited = _visitedLinks.contains(widget.uri);
 
   @override
   Widget build(BuildContext context) {
     final effectiveTextStyle = widget.textStyle ?? context.typography.base.bodyMedium;
-    final effectiveColor = _hover ? (widget.hoverTextColor ?? context.colors.accent) : (widget.textColor ?? context.colors.contentMedium);
+    final effectiveColor = _visited
+        ? (widget.visitedTextColor ?? context.colors.accent)
+        : _hover
+            ? (widget.hoverTextColor ?? context.colors.contentMediumHover)
+            : (widget.textColor ?? context.colors.contentMedium);
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       onEnter: (_) => setState(() => _hover = true),
       onExit: (_) => setState(() => _hover = false),
       child: GestureDetector(
-        onTap: () => launchUrl(widget.uri),
-        onTertiaryTapUp: (_) => launchUrl(widget.uri, webOnlyWindowName: webBlank),
+        onTap: () {
+          setState(() => _visited = true);
+          launchUrl(widget.uri).whenComplete(() => _visitedLinks.add(widget.uri));
+        },
+        onTertiaryTapUp: (_) {
+          setState(() => _visited = true);
+          launchUrl(widget.uri, webOnlyWindowName: webBlank).whenComplete(() => _visitedLinks.add(widget.uri));
+        },
         child: AbsorbPointer(
           child: Text(
             widget.text,
             style: effectiveTextStyle.apply(
-              decoration: (widget.underlineHover && _hover) ? TextDecoration.underline : null,
+              decoration: (widget.underlineVisited && _visited) || (widget.underlineHover && _hover) ? TextDecoration.underline : null,
               decorationColor: effectiveColor,
               color: effectiveColor,
             ),
