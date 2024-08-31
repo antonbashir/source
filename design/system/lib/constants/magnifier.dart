396a12cc3d965d5eee2960c487358d641ec44ba3
Anton Bashirov <anton.sh.local@gmail.com>
2024-08-31 03:00:38 +0300

    Update 2024-08-31

--- design/system/lib/constants/magnifier.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-const magnifierSize = Size(77.37, 37.9);
-const magnifierStandardVerticalFocalPointShift = 22;
-const magnifierBorderRadius = 40.0;
-const magnifierMagnification = 1.25;
-const magnifierFilmColor = Color.fromARGB(8, 158, 158, 158);
-const magnifierShadows = <BoxShadow>[BoxShadow(blurRadius: 1.5, offset: Offset(0, 2), spreadRadius: 0.75, color: Color.fromARGB(25, 0, 0, 0))];
-const magnifierJumpBetweenLinesAnimationDuration = Duration(milliseconds: 70);
--- design/system/lib/constants/shimmer.dart
+++
@@ -1,1 +0,0 @@
-enum ShimmerDirection { leftToRight, rightToLeft, topToBottom, bottomToTop }
